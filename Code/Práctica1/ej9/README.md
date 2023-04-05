ce9c061e81679ca978896475b72ac7b5247073a8
Joni Bekenstein <joni@bek.io>
2023-04-05 17:48:23 -0300

    Agrega ej9

---
+++ Code/Práctica1/ej9/README.md
@@ -0,0 +1,8 @@
+# Práctica 1 / Ejercicio 9
+
+No hay argumentos de entrada, la matriz está definida en el código.
+
+```bash
+make
+./bin/app
+```
---
+++ Code/Práctica1/ej9/main.cpp
@@ -0,0 +1,49 @@
+#include <limits>
+#include <math.h>
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+int m = 3;
+int n = 3;
+vector<vector<int>> A = {
+    {-2,  -3,  3},
+    {-5, -10,  1},
+    {10,  30, -5},
+};
+
+vector<vector<int>> memo(m, vector<int>(n, -1));
+int f(int i, int j) {
+    // When we reach this position outside A we treat it as the base case from where
+    // to build up the required life. We return 1 because that's the minimum life.
+    if (i == m && j == n - 1) return 1;
+
+    // Any other position outside A return +infinity.
+    if (!(0 <= i && i < m && 0 <= j && j < n)) return numeric_limits<int>::max();
+
+    if (memo[i][j] == -1) {
+        memo[i][j] = max(1, min(f(i + 1, j), f(i, j + 1)) - A[i][j]);
+    }
+
+    return memo[i][j];
+}
+
+int main(int argc, char *argv[]) {
+    // Print matrix.
+    printf("A:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (A[i][j] >= 0) printf(" ");
+            printf("%d", A[i][j]);
+            if (j < n - 1) printf("\t");
+        }
+        printf("\n");
+    }
+
+    // Calculate solution.
+    int res = f(0, 0);
+    printf("solution: %d\n", res);
+
+    return 0;
+}
